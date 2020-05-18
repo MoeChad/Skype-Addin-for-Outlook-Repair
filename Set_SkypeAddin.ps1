891e4ce94739c9827a77517d72a38d0f191f2c51
Moe <57568144+MoeChad@users.noreply.github.com>
2020-05-17 20:15:18 -0400

    Update Set_SkypeAddin.ps1

--- Set_SkypeAddin.ps1
+++ Set_SkypeAddin.ps1
@@ -19,7 +19,7 @@ function Set-SkypeAddin {
     New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS > $null 
 
     $ResiliencyPath = 'HKU:\{0}\Software\Microsoft\Office\16.0\Outlook\Resiliency\DoNotDisturbAddinList' -f $SID
-    $AddinPath = 'HKU:\{0}\Software\Micrsoft\Office\16.0\Outlook\Addins\UCAddin.LyncAddin.1' -f $SID
+    $AddinPath = 'HKU:\{0}\Software\Microsoft\Office\16.0\Outlook\Addins\UCAddin.LyncAddin.1' -f $SID
 
     if ((Test-Path -Path $ResiliencyPath) -eq $false)  {
         Write-Verbose -Message ('The user path for DoNotDisturbAddinList does not exist for {0}; creating that path' -f $UserName)
